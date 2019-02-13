ca9a40172a9f4f1fcfe238ba4e0c47b148a2a98b
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-02-12 21:08:33 -0500

    fix: force "text" reporting of test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,10 @@ const defaultSettings = require('@open-wc/testing-karma/default-settings.js');
 const merge = require('webpack-merge');
 
 module.exports = config => {
+  const defaultConfig = defaultSettings(config);
+  defaultConfig.coverageIstanbulReporter.reports = ['html', 'lcovonly', 'text'];
   config.set(
-    merge(defaultSettings(config), {
+    merge(defaultConfig, {
       files: [
         // allows running single tests with the --grep flag
         config.grep ? config.grep : 'test/**/*.test.js',
